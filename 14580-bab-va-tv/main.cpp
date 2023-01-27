f34a93bceb507d92bc66329fd65de8a81f15cf34
sajad <salimzadehsajad@gmail.com>
2023-01-27 23:00:01 +0330

    14580

--- 14580-bab-va-tv/main.cpp
+++ 14580-bab-va-tv/main.cpp
@@ -3,6 +3,17 @@
 using namespace std;
 
 int main() {
+    int n, k, x;
+    cin >> n >> k;
+    cin >> x;
+    string names[n];
+    for (int i = 0; i < n; ++i) {
+        cin >> names[i];
+    }
 
+    int c = (x + k);
+    if(c > n) c--;
+    c = c % n;
+    cout << names[c];
     return 0;
 }
